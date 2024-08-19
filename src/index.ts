7496879be768b5e73f0166e50d5e8e15ded3876b
GabrielPedrotti <gabrielps2003@gmail.com>
2024-08-19 13:26:13 -0300

    fix: return .js

--- src/index.ts
+++ src/index.ts
@@ -3,8 +3,8 @@ import { registerPlugin } from '@capacitor/core';
 import type { UsbSerialPlugin } from './definitions';
 
 const UsbSerial = registerPlugin<UsbSerialPlugin>('UsbSerial', {
-  web: () => import('./web').then((m) => new m.UsbSerialWeb()),
+  web: () => import('./web.js').then((m) => new m.UsbSerialWeb()),
 });
 
-export * from './definitions';
+export * from './definitions.js';
 export { UsbSerial };
